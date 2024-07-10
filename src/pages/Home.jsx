bfb03ba9658417b95e989cee457d16b6ef703b2e
FurqanLatif <furqanlatif200@gmail.com>
2024-07-10 19:56:16 +0500

    push

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,17 +3,16 @@ const Home = () => {
     <>
       {/* Home Logo */}
       <section>
-        <div className="logo sm:pt-2 flex justify-center items-center">
-          <div className="logo-main w-full mx-auto sm:pt-6">
+      <div className="flex justify-center items-center">
             <a href="#">
-              <img
-                className="mx-auto block sm:w-1/12 md:w-4/12 lg:w-3/12"
+            <img
+                className="mx-auto block sm:w-10/12 md:w-8/12 lg:w-6/12 xl:w-4/12"
                 src="https://yousave.ai/img/new-logo.webp"
                 alt="Logo"
               />
             </a>
-          </div>
-        </div>
+      </div>
+
         <div className="text-center sm:pt-2 md:pt-12 lg:pt-12">
           <h3 className="font-montserrat text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold leading-[61px]">
             Save Money, Save Faster
@@ -24,10 +23,10 @@ const Home = () => {
         </div>
         <div className="pt-12">
           <form action="result.html" className="md:block" method="get">
-            <div className="relative w-3/4 sm:w-6/12  mx-auto h-14 rounded-full border-2 border-orange-280 flex justify-between shadow-[0_0_0_2.5px_rgba(255,92,0,1)]">
+            <div className="relative w-3/4 sm:w-6/12 mx-auto h-14 rounded-full border-2 border-orange-280 flex justify-between shadow-[0_0_0_2.5px_rgba(255,92,0,1)]">
               <input
                 required
-                className="w-[calc(100%-60px)] max-w-full h-full border-none rounded-full pl-3 pr-3 text-lg font-montserrat"
+                className="w-full h-full border-none rounded-full pl-3 pr-10 text-lg font-montserrat placeholder-gray-500"
                 type="text"
                 placeholder="Search over 2 billion products"
                 name="search_query"
@@ -35,7 +34,7 @@ const Home = () => {
               />
               <button
                 type="submit"
-                className="border-none rounded-r-full bg-transparent cursor-pointer flex items-center text-orange-500 h-full"
+                className="border-none rounded-full bg-transparent cursor-pointer flex items-center justify-center text-orange-500 w-14 h-14 absolute right-0 top-0"
               >
                 <i className="fas fa-search"></i>
               </button>
