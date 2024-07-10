95e36383e0e9fb99aec599cc97e4bafe9f2c1915
AnasMansha <usamak657@gmail.com>
2024-07-11 00:13:39 +0500

    sample screen size code

--- src/components/home-components/Banner.jsx
+++ src/components/home-components/Banner.jsx
@@ -1,20 +1,23 @@
 const HomeBanner = () => {
-    return (
-      <>
+  return (
+    <>
       <section className="">
-          <img
-              className="hidden md:block w-full" screens
-              src="https://yousave.ai/img/432.svg"
-              alt=""
-          />
-          <img
-              className="block md:hidden" screens
-              src="https://yousave.ai/img/background-mobile.webp"
-              alt=""
-              style={{ width: '100%', height: '38vw' }}
-          />
+        <img
+          className="hidden md:block w-full"
+          screens
+          src="https://yousave.ai/img/432.svg"
+          alt=""
+        />
+        <img
+          className="block md:hidden"
+          screens
+          src="https://yousave.ai/img/background-mobile.webp"
+          alt=""
+          style={{ width: "100%", height: "38vw" }}
+        />
       </section>
-      </>
-    )}
+    </>
+  );
+};
 
-    export default HomeBanner;
+export default HomeBanner;
