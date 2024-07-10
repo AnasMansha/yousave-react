bde1032e482eebd4327c3e2dcc94d9d82d1efe4f
AnasMansha <usamak657@gmail.com>
2024-07-11 01:09:08 +0500

    axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,16 @@ import HomeBanner from "components/home-components/Banner";
 import ComparePrice from "components/home-components/Compareprice";
 import ThreeClickSection from "components/home-components/ThreeClickSection";
 import Footer from "components/Footer";
+import { useEffect } from "react";
+import { search } from "utils/apis/product";
 const Home = () => {
+  const searchProduct = async () => {
+    const response = await search("Mango");
+    debugger;
+  };
+  useEffect(() => {
+    searchProduct();
+  }, []);
   return (
     <>
       <Home_header />
